da3313dccce4f8a3116a2e418915a64670999a44
Lueas antel <76736461+lueasantel@users.noreply.github.com>
2022-01-20 10:20:17 +0700

    Add files via upload
    add commit

---
+++ javascript/script.js
@@ -0,0 +1,26 @@
+var myVar;
+function myFunction() {
+  myVar = setTimeout(showPage, 700);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+
+function myFunctionsearch() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("searchBar");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myLIB");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
